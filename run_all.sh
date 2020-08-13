e37a5e5860a18b5a51702c207278034596754ef0
Vasicek,Ondrej <ondrej.vasicek@honeywell.com>
2020-08-13 11:23:03 +0200

    added log files to run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-cd "${BASH_SOURCE%/*}"
+USRPATH=$PWD # get the call directory
+cd "${BASH_SOURCE%/*}" # move to the script directory
 
+# catch ctrl+c and kill all subprocesses
 trap 'killall' INT
-
 killall() {
     trap '' INT TERM     # ignore INT and TERM while shutting down
     echo "Shutting down..."
@@ -13,21 +14,25 @@ killall() {
     exit
 }
 
+# append headings to .log files
+CURTIME=$(date)
+echo -e "\n####################################################n## New run started at: $CURTIME" >> $USRPATH/triplestore.log
+echo -e "\n####################################################n## New run started at: $CURTIME" >> $USRPATH/compilation.log
+echo -e "\n####################################################n## New run started at: $CURTIME" >> $USRPATH/analysis.log
 
+# start all 3 processes
 echo "Starting the Triplestore"
 cd sparql_triplestore
-./run.sh 2>&1 | grep "Started ServerConnector" &
+./run.sh 2>&1 | tee -a $USRPATH/triplestore.log | grep "Started ServerConnector" &
 
 echo "Starting the Compilation adapter"
 cd ../compilation
-mvn jetty:run-exploded 2>&1 | grep "Started ServerConnector" &
+mvn jetty:run-exploded 2>&1 | tee -a $USRPATH/compilation.log | grep "Started ServerConnector" &
 
 echo "Starting the Analysis adapter"
 cd ../analysis
-mvn jetty:run-exploded 2>&1 | grep "Started ServerConnector" &
+mvn jetty:run-exploded 2>&1 | tee -a $USRPATH/analysis.log | grep "Started ServerConnector" &
 
-echo
-echo "Wait till startup finishes (3 messages with address:host at the end)"
-echo "Use ctrl+c to exit..."
+echo -e "\nWait till startup finishes (3 messages with address:host at the end)\nUse ctrl+c to exit..."
 
 cat # wait forever
