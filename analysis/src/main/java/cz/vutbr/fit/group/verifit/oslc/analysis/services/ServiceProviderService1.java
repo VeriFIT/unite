745621996820fd8ac1e16acfb5b9e5f626ed327b
ivasicek <ivasicek@fit.vut.cz>
2022-01-27 09:55:50 +0100

    added an option to execute everyting using CMD instead of PS1 on Win

--- analysis/src/main/java/cz/vutbr/fit/group/verifit/oslc/analysis/services/ServiceProviderService1.java
+++ analysis/src/main/java/cz/vutbr/fit/group/verifit/oslc/analysis/services/ServiceProviderService1.java
@@ -86,6 +86,7 @@ import org.eclipse.lyo.oslc4j.core.model.Link;
 import org.eclipse.lyo.oslc4j.core.model.AbstractResource;
 
 import cz.vutbr.fit.group.verifit.oslc.analysis.VeriFitAnalysisManager;
+import cz.vutbr.fit.group.verifit.oslc.analysis.properties.VeriFitAnalysisProperties;
 import cz.vutbr.fit.group.verifit.oslc.analysis.VeriFitAnalysisConstants;
 import org.eclipse.lyo.oslc.domains.auto.Oslc_autoDomainConstants;
 import org.eclipse.lyo.oslc.domains.auto.Oslc_autoDomainConstants;
@@ -136,7 +137,7 @@ public class ServiceProviderService1
     )
     public Response getOS()
     {
-    	String os = SystemUtils.IS_OS_LINUX ? "linux" : "windows"; // TODO assumes that "not linux" means "windows"
+    	String os = VeriFitAnalysisProperties.CONFIG_OS_STR;
 		return Response.status(200).entity(os).build();
     }
     // End of user code
