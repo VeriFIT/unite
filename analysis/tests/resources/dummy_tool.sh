faee237b17d0d151322f0d695c1311f91f8554af
ivasicek <ivasicek@fit.vut.cz>
2022-02-03 15:07:13 +0100

    made the analysis test suite faster
    dummy tool now has a new parameter to configure its sleep duration

--- analysis/tests/resources/dummy_tool.sh
+++ analysis/tests/resources/dummy_tool.sh
@@ -1,9 +1,9 @@
 #/bin/sh
 
 echo "Dummy tool!"
-sleep 5
-echo "Your argument: $1"
-echo "SUT: $2"
+sleep $1 1> /dev/null 2> /dev/null
+echo "Your argument: $2"
+echo "SUT: $3"
 echo "test file" > file1
 mkdir -p dir1
 echo "test file" > dir1/file2
