33f3e5b722431b61bfaddf1beaf593aa24600689
ivasicek <ivasicek@fit.vut.cz>
2022-05-27 18:10:57 +0200

    added test for ansi

--- shared/src/main/java/cz/vutbr/fit/group/verifit/oslc/shared/utils/Utils.java
+++ shared/src/main/java/cz/vutbr/fit/group/verifit/oslc/shared/utils/Utils.java
@@ -395,7 +395,7 @@ public class Utils {
 	public static String removeAnsiSequences(String strToProcess)
 	{  
 		return strToProcess.replaceAll(
-		  "[\\u001b\\u009b][^m]*?m"
+		  "[\\u001b\\u009b][^m]*?m"	// TODO maybe get a better regex? the below did not work
 		//"[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]+)*|[a-zA-Z\\d]+(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?\\u0007)|(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-nq-uy=><~]"
 						,"");
 	}
